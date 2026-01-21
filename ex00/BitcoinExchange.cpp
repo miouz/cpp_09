a9025ca99f5f69416bb5c6b0eeb6bc02a820f50f
Zhou Mi <mizhouis@gmail.com>
2026-01-22 00:31:51 +0100

    update(ex00): update files

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -15,26 +15,78 @@ void BitcoinExchange::setDataBase(const std::string& fileName)
 {
 	std::fstream file(fileName, std::ios::in);
 	std::string	line;
+	size_t	lineNum = 0;
 
 	if (file.is_open() == false)
 		throw std::invalid_argument("could not open file.");
 	while (std::getline(file, line))
 	{
+		lineNum++;
+		if (lineNum == 1)
+			continue;
 		if (isValidDataLine(line) == false)
-			std::cout << "line " << dataBase_.size() << " format is not correct: "
+			std::cout << "line " << lineNum << " format is not correct: "
 				<< line <<" \" continued" << "\n";
 		else
 			addLineToDataBase(line);
 	}
 }
 
+bool isValidDate(std::string& date)
+{
+	try {
+		std::string year = date.substr(0, 4);
+		std::string month = date.substr(6, 8);
+		std::string day = date.substr(10, 12);
+		if (*(date.begin()+ 5) == '-'
+			&& *(date.begin()+ 9) == '-'
+			&& year >= "2009" && year <= "2999"
+			&& month >= "01" && month <= "12"
+			&& day >= "01" && day <= "31")
+			return true;
+		return false;
+	} catch (...) {
+		return false;
+	}
+}
+
 bool BitcoinExchange::isValidDataLine(const std::string& line) const
 {
-	std::string::iterator = line.find('|');
+	size_t commaPos = line.find(",");
+	if (commaPos == std::string::npos)
+		return false;
+	std::string date = line.substr(0, commaPos);
+	std::string rate = line.substr(commaPos + 1);
 
+		if (isValidDate(date) == true
+			&& isValidRate(rate) == true)
+			return true;
+	return false;
 
 }
 void BitcoinExchange::addLineToDataBase(const std::string& line)
 {
 
+}
+
+void BitcoinExchange::displayValueFromLine(const std::string& line) const
+{
+
+}
+
+void BitcoinExchange::displayValueFromFile(const std::string& fileName) const
+{
+	std::ifstream input(fileName.c_str(), std::ios::in);
+	std::string	line;
+
+	if (!input.is_open())
+		throw std::runtime_error("couldn't open input file");
+	while (std::getline(input, line))
+	{
+		try {
+		displayValueFromLine(line);
+		} catch (std::exception& e) {
+			std::cout << "Error: " << std::endl;
+		}
+	}
 }
