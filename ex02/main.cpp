b4126766e88896f8e87e467927cfa5c9eab58156
Zhou Mi <mizhouis@gmail.com>
2026-02-03 20:39:27 +0100

    update(ex02): update main to initialize vector efficiently

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -12,23 +12,20 @@ int getNumber(char *str)
 	return static_cast<int>(result);
 }
 
-std::vector<int> getNumsFromArg(int argc, char **argv)
+void getNumsFromArg(int argc, char **argv, std::vector<int>& nums)
 {
 	if (argc < 2)
 		throw std::invalid_argument("need arguments");
-	std::vector<int> nums(argc - 1);
+	nums.reserve(argc - 1);
 	for (std::size_t i = 1; argv[i]; i++)
-	{
-		std::size_t indexVec = i - 1;
-		nums[indexVec] = getNumber(argv[i]);
-	}
-	return nums;
+		nums.push_back(getNumber(argv[i]));
 }
 
 int main(int argc, char** argv)
 {
 	try {
-		std::vector<int> nums (getNumsFromArg(argc, argv));
+		std::vector<int> nums;
+		getNumsFromArg(argc, argv, nums);
 		PmergeMe::recursiveSort(nums, 1);
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
