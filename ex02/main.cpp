5ece49f2da7ec22575581c547cc3b698a82bf67f
Mi Zhou <mzhou@student.42.fr>
2026-02-18 15:26:16 +0100

    update(ex02): correct print messages

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -19,32 +19,27 @@ int main(int argc, char** argv)
 		count = 0;
 		std::vector<int> numsVector;
 		PmergeMe::getNumsFromArg(argc, argv, numsVector);
-		std::cout << argc - 1 <<" integers before sorting :\n";
+		std::cout <<"Before: ";
 		PmergeMe::printContent(numsVector);
 		double startVecTime = getUtime();
 		PmergeMe::recursiveSort(numsVector, 1);
 		double vecTime = getUtime() - startVecTime;
-		std::cout << "sorted with [std::vector<int>] :\n";
+		std::cout <<"After:  ";
 		PmergeMe::printContent(numsVector);
 		std::cout << "comparaison times by using jacobsthal sequence: "
 			<< count << "\n"
-			<< "time to process sorting: " << vecTime << " microseconds"<< "\n";
-		std::cout << "\n===================================================\n\n";
+			<< "Time to process a range of " << argc -1 
+			<< " elements with [ std::vector<int> ]" << vecTime << " microseconds"<< "\n";
 
 
 		count = 0;
 		std::deque<int> numsDeque;
 		PmergeMe::getNumsFromArg(argc, argv, numsDeque);
-		std::cout << argc - 1 << " integers before sorting :\n";
-		PmergeMe::printContent(numsDeque);
 		double startDeqTime = getUtime();
 		PmergeMe::recursiveSort(numsDeque, 1);
 		double deqTime = getUtime() - startDeqTime;
-		std::cout << "sorted with [std::deque<int>] :\n";
-		PmergeMe::printContent(numsDeque);
-		std::cout << "comparaison times by using jacobsthal sequence: "
-			<< count << "\n"
-			<< "time to process sorting: " << deqTime << " microseconds"<< "\n\n";
+		std::cout << "Time to process a range of " << argc -1 
+			<< " elements with [ std::deque<int> ]" << deqTime << " microseconds"<< "\n";
 
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
