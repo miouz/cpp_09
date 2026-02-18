09f9334e089724dd6d36490c5a0f784056b80484
Mi Zhou <mzhou@student.42.fr>
2026-02-18 15:14:30 +0100

    add(ex02): add test.sh

--- ex02/test.sh
+++ ex02/test.sh
@@ -10,7 +10,7 @@ CYAN='\033[0;36m'
 WHITE='\033[0;37m'
 RESET='\033[0m'
 
-PROGRAM="./RPN"
+PROGRAM="./PmergeMe"
 
 if [ ! -f $PROGRAM ]; then
 	printf "${RED} Error: $PROGRAM do not exit\n${RESET}"
@@ -18,11 +18,12 @@ if [ ! -f $PROGRAM ]; then
 fi
 
 run_test(){
-	local input="$1"
-	local discription="$2"
-	local output=$($PROGRAM "$input")
+	local discription="$1"
+	shift
+	local args="$@"
+	local output="$($PROGRAM $args 2>&1)"
 
-	printf "${YELLOW}Test: ${GREEN}[$input] Case: [$discription]\n${RESET}"
+	printf "${YELLOW}Test: ${GREEN}[$discription] Case: [$args]\n${RESET}"
 	printf "%s\n" "$output"
 	printf "\n"
 }
@@ -31,18 +32,16 @@ printf "${GREEN}==========NO ARGUMENTS TEST=============${RESET}\n"
 $PROGRAM
 
 printf "${GREEN}==========INVALID ARGUMENTS TEST=============${RESET}\n"
-run_test "" "empty string test"
-run_test " a b + " "invalid input: no digit"
-run_test " 100 10 +" "invalid input: more than 1 digit"
-run_test " + + " "invalid input: operator before number"
-run_test "-1 -3 * " "negatif one digit"
+run_test "empty string" ""
+run_test "invalid input" 10 ab 900 3
+run_test "negatif integer" 100 -1 -3
+run_test "integer overflow" 1 0 2147483648
 
-printf "${GREEN}==========CALCULATE ERROR TEST=============${RESET}\n"
-run_test "1 0 / " "division by zero"
-run_test "1 0 0 + / " "division by zero"
-run_test "9 9 * 9 * 9 * 9 * 9 * 9 * 9 * 9 * 9 * 9 * 9 * 9 * 9 * 9 * 9 *" "int overflow"
-
-
-printf "${GREEN}==========NO ERROR TEST=============${RESET}\n"
-run_test "5 1 2 + 4 * + 3 -" "expected: 14"
-run_test "1 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 +" "expected 20"
+printf "${GREEN}==========SORTING NO ERROR=============${RESET}\n"
+run_test "1 integer" 0
+run_test "2 integers" 98 25
+run_test "3 integers" 692 253 0
+run_test "4 integers" 920 0 45 890
+run_test "7 integers" 5 20 0 987654 258 36 7
+run_test "copies" 0 9 456 0 0 9 2
+run_test "3000 numbers" `shuf -i 1-100000 -n 3000 | tr "\n" " "`
